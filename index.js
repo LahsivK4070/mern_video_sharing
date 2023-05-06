e727bb0c9f9358711134b1711a29934f3080fab4
Vishal Kumar <vishalkumarcoold2@gmail.com>
2023-05-07 00:25:41 +0530

    updated package.json in client

--- index.js
+++ index.js
@@ -20,13 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 const connect = () => {
     mongoose.set('strictQuery', true);
